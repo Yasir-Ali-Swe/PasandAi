1109921d038af66b16160271a82f0b784fff9c25
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2026-09-17 00:58:02 +0500

    add server

---
+++ server/src/server.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import app from "./app.js";
+
+const PORT = Number(process.env.PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
